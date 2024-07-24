ca155fae05e48e58be49f1626b741b2c50c4db56
Joona Laitila <joona.laitila123@hotmail.com>
2024-07-24 15:27:27 +0300

    added code

--- routes/route_foods.py
+++ routes/route_foods.py
@@ -5,62 +5,9 @@ import requests
 bp_foods = Blueprint('bp_foods', __name__)
 
 
-@bp_foods.route('/fineli/params', methods=['POST'])
-def fineliParams():
-    params = request.json
-    print(params)
 
-    return '{"msg": "OK"}'
-
-@bp_foods.route('/api/foods/fetch', methods=['GET'])
-def fetch_food():
-    
-    return '{"msg": "OK"}'
-
-
-
-@bp_foods.route('/search', methods=['GET', 'POST'])
+@bp_foods.route('/fineli/search', methods=['GET', 'POST'])
 def search():
-    data = request.json
-    
-    if not data or 'name' not in data:
-        return jsonify({'error': 'No name provided'}), 400
-
-    query = data['name']
-    
-    params = {'q': query}
-
-    url = 'https://fineli.fi/fineli/api/v1/foods'
-
-    headers = {
-        'User-Agent': 'Dietgen',
-        'Authorization': 'Bearer YOUR_API_KEY'
-    }
-
-    try:
-        response = requests.get(url, params=params, headers=headers)
-        
-        response.raise_for_status()
-
-        api_data = response.json()
-        
-        return jsonify(api_data)
-    
-    except requests.exceptions.RequestException as e:
-        return jsonify({'error': str(e)}), 500
-    
-
-# @bp_foods.route('/fineli/testi', methods=['POST'])
-# def fineliTest():
-#     paramsss = request.json
-#     print(paramsss)
-
-#     return jsonify(paramsss)
-
-
-
-@bp_foods.route('/fineli/testi', methods=['GET', 'POST'])
-def search1():
     data = request.json
     print(data['data'])
     
@@ -98,3 +45,40 @@ def search1():
     
     except requests.exceptions.RequestException as e:
         return jsonify({'error': str(e)}), 500
+
+
+
+
+@bp_foods.route('/fineli/food/<id>', methods=['GET'])
+def getFoodData(id):
+    food_id = id
+    url = "https://fineli.fi/fineli/api/v1/foods/"
+    headers = {
+        'User-Agent': 'Dietgen',
+        'Authorization': 'Bearer YOUR_API_KEY'
+    }
+
+    try:
+        response = requests.get(url + food_id, headers=headers)
+        
+        response.raise_for_status()
+
+        
+
+        api_data = response.json()
+
+        
+        return api_data
+    
+    except requests.exceptions.RequestException as e:
+        return jsonify({'error': str(e)}), 500
+
+
+
+
+
+    
+
+    
+
+    return '{"msg": "OK"}'
