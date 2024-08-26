5e876a92bee15f6cdba568082e6865776598ed86
Jani <n4512@student.jamk.fi>
2024-08-26 15:52:21 +0300

    Update config.py to use environment variables for database credentials

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 class Config(object):
+    DB_USERNAME = os.getenv('DB_USERNAME')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_HOST = 'localhost'
+    DB_NAME = 'dietgen_db'
     SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/dietgen_db'
+    #SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
     JWT_SECRET_KEY = "sala"
