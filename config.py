1eb878d14073b9fb35422408cc75728382fadf44
Jani <n4512@student.jamk.fi>
2024-09-04 17:44:14 +0300

    connection string

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ class Config(object):
     DB_PASSWORD = os.getenv('DB_PASSWORD')
     DB_HOST = 'localhost'
     DB_NAME = 'dietgen_db'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/dietgen_db'
-    #SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+    #SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/dietgen_db'
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
     JWT_SECRET_KEY = "sala"
